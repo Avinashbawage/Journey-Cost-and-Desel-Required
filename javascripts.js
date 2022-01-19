4fecf4f4ceeeb3e971f09f55d8625b88d1d2906e
Avinash Bawage <84569613+Avinashbawage@users.noreply.github.com>
2022-01-20 00:52:32 +0530

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,43 @@
+// Sum of Two Numbers
+function sum() {
+  let a = parseInt(document.getElementById("a").value);
+  let b = parseInt(document.getElementById("b").value);
+  let c = a / b;
+  let e = parseInt(document.getElementById("ef").value);
+
+  let k = c * e;
+
+  document.getElementById("demo").innerHTML = c;
+  document.getElementById("fv").innerHTML = k;
+
+  
+}
+
+// const biodata = {
+//   name: "Avinash",
+//   age: "26",
+//   born: "bidar",
+// };
+
+// const { name, age, born } = biodata;
+
+// let show = "Hi I am Avinash ",
+//   Degree = "B.E.";
+
+// // uagbujhbsdau
+
+// let showvalue = `My name is Avinash. My age is ${age}.I am born in ${born}`;
+
+// document.getElementById("showvaluedata").innerHTML = showvalue;
+
+// function funages(age = 18) {
+//   return age;
+// }
+
+// // function addpay(a, b) {
+// //   return a + b;
+// // }
+
+// let addpays = (a, b) => {
+//   return a + b;
+// };
